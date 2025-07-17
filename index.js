d976333180fe1a752b09096ad43175bf6ef82b44
Sailee30 <saileechavan04@gmail.com>
2025-07-17 23:41:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mysql2 = require('mysql2');
 const cors = require('cors');
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const con= mysql2.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'abc123',
-	database: 'qg_june25'
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
 });
 
 con.connect(err => {
